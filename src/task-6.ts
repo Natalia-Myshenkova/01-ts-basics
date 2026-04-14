aded15086149cac5745cdae58e70a8928f03cdc1
Natalia <natamis6122@gmail.com>
2026-04-14 20:19:00 +0300

    add new 6 task

--- src/task-6.ts
+++ src/task-6.ts
@@ -2,6 +2,6 @@ function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
-getFirstElement<number>([1, 2, 3]);
-getFirstElement<string>(["a", "b", "c"]);
-getFirstElement<boolean>([true, false]);
+getFirstElement<number>([1, 2, 3]);           
+getFirstElement<string>(["a", "b", "c"]);     
+getFirstElement<boolean>([true, false, true]);
